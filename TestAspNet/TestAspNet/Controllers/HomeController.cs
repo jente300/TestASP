bb286efa1f5384ec2640c96fcf536f2ff975b2bf
jente300 <73303006+jente300@users.noreply.github.com>
2023-01-19 11:19:28 +0100

    Add files via upload

---
+++ TestAspNet/TestAspNet/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestAspNet.Models;
+
+
+namespace TestAspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
